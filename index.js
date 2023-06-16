4023eec71e49675eec577a0de8e596dd45692858
renrojas <karensadithrojas@gmail.com>
2023-06-16 13:45:21 -0600

    added array of questions for README.md

--- index.js
+++ index.js
@@ -12,15 +12,51 @@ const questions = [
   },
   {
     type: 'input',
-    message: 'What languages do you know?',
-    name: 'languages'
+    message: 'What was your motivation? Why did you build this project? What problem does it solve? What did you learn?',
+    name: 'description'
+  },
+  {
+    type: 'input',
+    message: 'What are the steps required to install your project?',
+    name: 'installation'
+  },
+  {
+    type: 'input',
+    message: 'What are the instructions and examples for use of your project?',
+    name: 'usage'
+  },
+  {
+    type: 'input',
+    message: 'Who collaborated with you on this project? What are the links to tutorials you used for this project?',
+    name: 'credits'
   },
   {
     type: 'list',
-    message: 'What is your preferred method of communication?',
-    choices: ['phone', 'email', 'telekinesis'],
-    name: 'list'
+    message: 'What is the License you are using?',
+    choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License'],
+    name: 'license'
   },
+  {
+    type: 'input',
+    message: '',
+    name: 'badges'
+  },
+  {
+    type: 'input',
+    message: 'What are your projects features?',
+    name: 'features'
+  },
+  {
+    type: 'input',
+    message: 'What are the guidelines for other developers to contribute?',
+    name: 'howtocontribute'
+  },
+  {
+    type: 'input',
+    message: 'What are examples on how to run tests?',
+    name: 'tests'
+  },
+
 ];
 
 // TODO: Create a function to write README file
@@ -34,7 +70,7 @@ function init() {
     const template = generateMarkdown(response)
     console.log(template)
     fs.writeFile('./utils/README.md', template, (err) =>
-    err ? console.log(err) : console.log('Successfully created index.html!')
+    err ? console.log(err) : console.log('Successfully created README.md!')
   );
     })
 }
